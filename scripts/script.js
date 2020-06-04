fd1220ff6a52dd7559112139c3412f89355ce26e
Mikhail Chepelev <michaelchep@yandex.ru>
2020-06-04 17:31:55 +0300

    Добавил создание карточек при загрузке и из формы

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,35 @@ const placeFormCloser = placePopup.querySelector('.popup__close-button');
 const placeNameInput = placeForm.querySelector('.form__input_type_place-name');
 const placeLinkInput = placeForm.querySelector('.form__input_type_link');
 
+const cardsContainer = document.querySelector('.elements');
+
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 function openClosePopup(popup) {
     popup.classList.toggle('popup_opened');
 }
@@ -28,14 +57,40 @@ function profileFormHandler (evt) {
     // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    openClosePopup();
+    openClosePopup(profilePopup);
 }
 
-placeFormHandler() {
-  return true;
+function getCards (arrayOfCards) {
+  arrayOfCards.forEach((element) => {
+    addItem(cardsContainer, renderCard(element.name, element.link))});
 }
 
-//Слушатели закрытия/открытия, отправки формы
+function renderCard(name, link) {
+  const cardTemplate = document.querySelector('.card-template').content;
+  // клонируем содержимое тега template
+  const card = cardTemplate.cloneNode(true);
+  const cardTitle = card.querySelector('.element__image-title');
+  const cardImage = card.querySelector('.element__image');
+  cardTitle.textContent = name;
+  cardImage.src = link;
+  return card;
+}
+
+function addItem(container, item) {
+  container.append(item);
+}
+
+function placeFormHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+  // Вставьте новые значения с помощью textContent
+  addItem(cardsContainer, renderCard(placeNameInput.value,placeLinkInput.value));
+  openClosePopup(placePopup);
+}
+
+document.addEventListener("DOMContentLoaded", () => {getCards(initialCards)});
+
 profileFormCaller.addEventListener('click',() => {openClosePopup(profilePopup);});
 profileFormCloser.addEventListener('click', () => {openClosePopup(profilePopup);});
 profileForm.addEventListener('submit', profileFormHandler);
