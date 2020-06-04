5b8b3b8ef5bea28be460a88883a262e58e20f18d
Mikhail Chepelev <michaelchep@yandex.ru>
2020-06-04 15:27:35 +0300

    Добавил форму места и обработчики открытия

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,45 @@
 // Находим форму в DOM
-const formElement = document.querySelector('.form');
-const formCaller = document.querySelector('.profile__edit-button');
-const formCloser = document.querySelector('.popup__close-button');
-const popup = document.querySelector('.popup');
-const nameInput = formElement.querySelector('.form__input_type_name');
-const jobInput = formElement.querySelector('.form__input_type_occupation');
+const profilePopup = document.querySelector('.popup_type_profile-edit');
+const profileForm = document.querySelector('.form_type_profile-edit');
+const profileFormCaller = document.querySelector('.profile__edit-button');
+const profileFormCloser = profilePopup.querySelector('.popup__close-button');
+const nameInput = profileForm.querySelector('.form__input_type_name');
+const jobInput = profileForm.querySelector('.form__input_type_occupation');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__occupation');
 
-function openClosePopup() {
-    if (popup.classList.contains('popup_opened') === false) {
-        nameInput.value = profileName.textContent; 
-        jobInput.value = profileJob.textContent;
-    } 
+const placeForm = document.querySelector('.form_type_place-add');
+const placePopup = document.querySelector('.popup_type_add-place');
+const placeFormCaller = document.querySelector('.profile__add-button');
+const placeFormCloser = placePopup.querySelector('.popup__close-button');
+const placeNameInput = placeForm.querySelector('.form__input_type_place-name');
+const placeLinkInput = placeForm.querySelector('.form__input_type_link');
+
+function openClosePopup(popup) {
     popup.classList.toggle('popup_opened');
 }
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
+function profileFormHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
     // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     openClosePopup();
 }
 
+placeFormHandler() {
+  return true;
+}
+
 //Слушатели закрытия/открытия, отправки формы
-formCaller.addEventListener('click', openClosePopup);
-formCloser.addEventListener('click', openClosePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+profileFormCaller.addEventListener('click',() => {openClosePopup(profilePopup);});
+profileFormCloser.addEventListener('click', () => {openClosePopup(profilePopup);});
+profileForm.addEventListener('submit', profileFormHandler);
+
+placeFormCaller.addEventListener('click', () => {openClosePopup(placePopup);});
+placeFormCloser.addEventListener('click', () => {openClosePopup(placePopup);});
+placeForm.addEventListener('submit', placeFormHandler);
